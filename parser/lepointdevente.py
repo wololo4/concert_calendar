20a046ba0fe548d92bc55793f627655a7a6d74ef
wololo4 <59884953+wololo4@users.noreply.github.com>
2026-08-18 00:56:40 -0400

    Log events found in lepointdevente.py
    Added print statement to log events found during processing.

--- parser/lepointdevente.py
+++ parser/lepointdevente.py
@@ -139,6 +139,7 @@ def parse_lpdv(config):
     cal = Calendar()
 
     for ev in all_events:
+        print(ev)
         title = ev["title"].lower()
         if any(re.search(a, title) for a in artists):
             print(f"Found LPDV event: {ev['title']} @ {ev['venue']}")
