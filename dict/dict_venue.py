23ee546a042d8aab774689c6064b46520f252754
wololo4 <59884953+wololo4@users.noreply.github.com>
2026-09-10 23:17:19 -0400

    Add venue aliases and city mappings

---
+++ dict/dict_venue.py
@@ -0,0 +1,55 @@
+VENUE_ALIASE = {
+    "L'Anti Bar": [
+        "L'Anti Bar",
+        "L'Anti Bar & Spectacles"
+    ],
+    "Club Soda": [
+        "Club Soda",
+        "CLUB SODA"
+    ],
+    "HISTORY": [
+        "History",
+        "HISTORY",
+        "History Toronto"
+    ],
+    "Salle Montaigne": [
+        "Salle Montaigne",
+        "Salle Montaigne - Cégep Limoilou"
+    ],
+    "Salle Wilfrid-Pelletier": [
+        "Salle Wilfrid-Pelletier",
+        "Wilfrid-Pelletier"
+    ],
+    "The Theatre at Great Canadian": [
+        "The Theatre at Great Canadian",
+        "The Theatre at Great Canadian Toronto"
+    ],
+}
+
+VENUE_CITY = {
+    "Club Soda": "Montreal",
+    "Fairmount Theatre": "Montreal",
+    "Le Ministère": "Montreal",
+    "MTELUS": "Montreal",
+    "L'Olympia": "Montreal", 
+    "Piranha Bar": "Montreal",
+    "Salle Wilfrid-Pelletier": "Montreal",
+    "Théâtre Beanfield": "Montreal",
+
+    "The Bronson": "Ottawa",
+    "Rainbow Bistro": "Ottawa",
+
+    "L'Anti Bar": "Quebec",
+    "L'Impérial": "Quebec",
+    "Salle Montaigne" : "Quebec", 
+    "La Source de la Martinière": "Quebec",
+    "Théâtre Capitole": "Quebec",
+
+    "The Danforth Music Hall": "Toronto",
+    "HISTORY": "Toronto",
+    "Lee's Palace": "Toronto",
+    "The Opera House": "Toronto",
+    "REBEL": "Toronto",
+    "The Theatre at Great Canadian": "Toronto",
+}
+
