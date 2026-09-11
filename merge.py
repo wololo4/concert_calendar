ac89472187505200e6ec813959c588aecff65679
wololo4 <59884953+wololo4@users.noreply.github.com>
2026-09-10 23:18:33 -0400

    Refactor venue data into separate module
    Removed hardcoded venue aliases and city mappings from merge.py.

--- merge.py
+++ merge.py
@@ -3,66 +3,12 @@ import re
 from icalendar import Calendar
 from feeds import load_feeds
 
+from dict.dict_venue import VENUE_ALIASE, VENUE_CITY
 from parser.ticketmaster import parse_ticketmaster_json
 from parser.progstorm import parse_progstorm_html, build_progstorm_calendar
 from parser.eventbrite import parse_eventbrite_html, build_eventbrite_calendar
 from parser.lepointdevente import parse_lpdv_html, build_event_lpdv
 from parser.imperialbell import parse_imperialbell_html, build_event_imperialbell
-
-VENUE_ALIASE = {
-    "L'Anti Bar": [
-        "L'Anti Bar",
-        "L'Anti Bar & Spectacles"
-    ],
-    "Club Soda": [
-        "Club Soda",
-        "CLUB SODA"
-    ],
-    "HISTORY": [
-        "History",
-        "HISTORY",
-        "History Toronto"
-    ],
-    "Salle Montaigne": [
-        "Salle Montaigne",
-        "Salle Montaigne - Cégep Limoilou"
-    ],
-    "Salle Wilfrid-Pelletier": [
-        "Salle Wilfrid-Pelletier",
-        "Wilfrid-Pelletier"
-    ],
-    "The Theatre at Great Canadian": [
-        "The Theatre at Great Canadian",
-        "The Theatre at Great Canadian Toronto"
-    ],
-}
-
-VENUE_CITY = {
-    "Club Soda": "Montreal",
-    "Fairmount Theatre": "Montreal",
-    "Le Ministère": "Montreal",
-    "MTELUS": "Montreal",
-    "L'Olympia": "Montreal", 
-    "Piranha Bar": "Montreal",
-    "Salle Wilfrid-Pelletier": "Montreal",
-    "Théâtre Beanfield": "Montreal",
-
-    "The Bronson": "Ottawa",
-    "Rainbow Bistro": "Ottawa",
-
-    "L'Anti Bar": "Quebec",
-    "L'Impérial": "Quebec",
-    "Salle Montaigne" : "Quebec", 
-    "La Source de la Martinière": "Quebec",
-    "Théâtre Capitole": "Quebec",
-
-    "The Danforth Music Hall": "Toronto",
-    "HISTORY": "Toronto",
-    "Lee's Palace": "Toronto",
-    "The Opera House": "Toronto",
-    "REBEL": "Toronto",
-    "The Theatre at Great Canadian": "Toronto",
-}
 
 def extract_city_from_event(ev):
     loc = ev.get("LOCATION")
